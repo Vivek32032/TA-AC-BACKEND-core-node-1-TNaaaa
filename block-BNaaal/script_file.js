1e59e304fac491c37d972d2f83b5f49c67fb04fe
Vivek Kumar <vivek32032@gmail.com>
2022-02-14 11:47:14 +0530

    add server.js file

---
+++ block-BNaaal/script_file.js
@@ -0,0 +1,8 @@
+var http = require("http");
+var server = http.createServer(handleRequest);
+function handleRequest(req,res){
+ res.end('welcome');
+}
+server.listen(4000,()=>{
+    console.log('listening on port 4000');
+})
